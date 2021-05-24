74cb745d54568edcce0686a16aa90bd14e02c440
Henrique Torres <henriquetfw@gmail.com>
2021-05-24 13:23:11 -0300

    Setup Flutter project

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SplitItWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Split.it",
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:split_it/app.dart';
+
+void main() {
+  runApp(SplitItWidget());
+}
